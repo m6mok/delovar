37e185a801f9c67f35f79a1273b04032d0f170fc
unknown <dea2003@bk.ru>
2023-04-28 20:15:09 +0300

    0.0.2

--- delovar/main/views.py
+++ delovar/main/views.py
@@ -1,8 +1,45 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def profile(request):
-    context = {}
+    context = {
+        'data': [
+            {
+                'pk': 0,
+                'name': '1',
+                'surname': '11',
+                'nickname': '111',
+            },
+            {
+                'pk': 1,
+                'name': '2',
+                'surname': '22',
+                'nickname': '222',
+            },
+            {
+                'pk': 2,
+                'name': '3',
+                'surname': '333',
+                'nickname': '',
+            }
+        ],
+        'documents': [
+            {
+                'name': 'Документ 1',
+                'url': 'https://ppnm1.ru/Prays_ot_01_04_23.pdf'
+            },
+            {
+                'name': 'Документ 2',
+                'url': ''
+            },
+            {
+                'name': 'Документ 3',
+                'url': ''
+            },
+        ]
+    }
     return render(request, 'main/profile.html', context)
 
 
